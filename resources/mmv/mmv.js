10666624656ff2b5741e39dd27ca2ce23496bec3
Mikael <lucas_mikael@yahoo.fr>
2019-07-18 14:56:56 +0200

    Fix direct access issue

--- resources/mmv/mmv.js
+++ resources/mmv/mmv.js
@@ -390,15 +390,17 @@
 
 		this.comingFromHashChange = !updateHash;
 
-		var filename = element.href.split('/');
-		filename = filename[filename.length-1];
-		if(element && filename.substr(0,3) == 'ia-'){
-			$.each( this.thumbs, function ( idx, thumb ) {
-				if ( thumb.title.getPrefixedText() === title.getPrefixedText() && thumb.thumb.src === element.href) {
-					viewer.loadImage( thumb.image, thumb.$thumb.clone()[ 0 ], true );
-					return false;
-				}
-			} );
+		if(element){
+			var filename = element.href.split('/');
+			filename = filename[filename.length-1];
+			if(filename.substr(0,3) === 'ia-'){
+				$.each( this.thumbs, function ( idx, thumb ) {
+					if ( thumb.title.getPrefixedText() === title.getPrefixedText() && thumb.thumb.src === element.href) {
+						viewer.loadImage( thumb.image, thumb.$thumb.clone()[ 0 ], true );
+						return false;
+					}
+				} );
+			}
 		} else {
 			$.each( this.thumbs, function ( idx, thumb ) {
 				if ( thumb.title.getPrefixedText() === title.getPrefixedText()) {
